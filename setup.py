e3a09e3cf27293cde85ab480c9bb52a3250bc815
tarepan <tarepan5884@gmail.com>
2022-01-08 01:22:25 +0900

    Add latest huggingface_hub

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ requirements = [
     "easydict",
     "catalyst",
     "sentencepiece",
-    "huggingface_hub @ git+https://github.com/huggingface/huggingface_hub.git#egg=huggingface_hub" # TODO: Replace with v0.0.17 when it is released
+    "huggingface_hub",
 ]
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
