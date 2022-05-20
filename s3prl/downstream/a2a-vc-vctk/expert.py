0998d195160e43a02d21a030ac7d0b259aab6f35
tarepan <tarepan5884@gmail.com>
2022-05-20 12:00:09 +0000

    [a2a-vc] Fix missing import

--- s3prl/downstream/a2a-vc-vctk/expert.py
+++ s3prl/downstream/a2a-vc-vctk/expert.py
@@ -26,6 +26,7 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import LambdaLR
 from resemblyzer import preprocess_wav, VoiceEncoder
+from omegaconf import MISSING
 
 from .model import Model, ConfModel, ConfDecoderMainNet
 from .networks.encoder import ConfEncoder
